a0022e758bbd45d809d6bdb02ff8e2f0117b14c9
Sarah10109 <hilybob10109@gmail.com>
2024-03-25 12:08:52 -0600

    added name and ucid

--- A4/my-ecommerce-app/src/App.js
+++ A4/my-ecommerce-app/src/App.js
@@ -1,3 +1,7 @@
+// File: App.js
+// Group members: Suprova Hoque (30174388), Sarah Elmahdy(30170100)
+// Date completed: March 25, 2024
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './component/Homepage'; // Adjust the import path as necessary
