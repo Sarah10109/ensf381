d8cd83cc21f50da97ff2e3a46c9855c1cebc0586
SuprovaHoque <123154933+SuprovaHoque@users.noreply.github.com>
2024-03-31 16:42:06 -0600

    Frontend code for authenticating login complete

--- A4 and A5/my-ecommerce-app/src/component/LoginForm.js
+++ A4 and A5/my-ecommerce-app/src/component/LoginForm.js
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = ({ switchToSignup }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     // Validate and handle login logic here
     if (username && password) {
       console.log("Logged in successfully!");
-      // Add your login logic (e.g., API calls, authentication, etc.)
       setErrorMessage(""); // Clear the error message
     } else {
       setErrorMessage("Please fill in both fields.");
     }
+
+    // Authenticating login
+    fetch('http://127.0.0.1:5000/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({'username': username, 'password': password})
+    }).then(response => response.json())
+    .then(response => {
+      if (response.authenticated) {
+        setAuthenticated(true);
+        setMessage("Login successful")
+      } else {
+        setAuthenticated(false);
+        setMessage('Login failed. Incorrect username or password.');
+      }
+    }).catch(error => {
+      console.error(error);
+      setMessage('An error occurred.');
+    });
+
   };
+
+  // Redirect to the products page after successful login
+  if(authenticated) {
+    navigate('/products')
+  }
 
   return (
     <div>
@@ -39,6 +67,7 @@ const LoginForm = ({ switchToSignup }) => {
       <button onClick={handleLogin}>Login</button>
       <br />
       <button onClick={switchToSignup}>Switch to Signup</button>
+      {message && <p style={{ color: "red" }}>{message}</p>}
     </div>
   );
 };
