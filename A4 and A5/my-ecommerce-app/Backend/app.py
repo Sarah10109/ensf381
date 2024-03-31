24ef9b9813b118da4f54a56c8d57037002385411
SuprovaHoque <123154933+SuprovaHoque@users.noreply.github.com>
2024-03-31 16:40:05 -0600

    Login and signup authentication complete

--- A4 and A5/my-ecommerce-app/Backend/app.py
+++ A4 and A5/my-ecommerce-app/Backend/app.py
@@ -3,9 +3,51 @@
 # import json
 # import os
 
-from flask import Flask
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS # Import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# List of user objects with username, password, and email
+users = [
+    {"username": "", "password": "", "email": ""}
+]
+
+@app.route('/login', methods=['POST'])
+def authenticate_user():
+    data = request.get_json()
+    entered_username = data.get('username')
+    entered_password = data.get('password')
+
+    # Check if the entered username and password match any user in the list
+    for user in users:
+        if user['username'] == entered_username and user['password'] == entered_password:
+            return jsonify({"authenticated": True, "message": "Login successful"})
+    return jsonify({"authenticated": False, "message": "Login failed. Incorrect username or password"})
+
+
+@app.route('/signup', methods=['POST'])
+def authenticate_signup():
+    signup_data = request.get_json()
+    username_created = signup_data.get('username')
+    password_created = signup_data.get('password')
+    email_created = signup_data.get('email')
+
+    # Check if the entered username matches any user in the list
+    for user in users:
+        if user['username'] == username_created:
+            return jsonify({"signupAuthenticated": False, "message": "Username already exists. Please choose a different username."})
+
+    # If the username doesn't match any user in the list, append the new user details to the users list
+    users.append({
+        "username": username_created,
+        "password": password_created,
+        "email": email_created
+    })
+    return jsonify({"signupAuthenticated": True, "message": "Signup successful"})
+
 
 @app.route('/')
 def hello():
