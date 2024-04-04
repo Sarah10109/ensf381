47f804aaea5fc8cf38f4ae82a5c84e3d705dbe5c
SuprovaHoque <123154933+SuprovaHoque@users.noreply.github.com>
2024-04-03 19:52:07 -0600

    Adjustments made to ensure ProductsList fetches from API in backend instead of products file

--- A4 and A5/my-ecommerce-app/src/component/ProductList.js
+++ A4 and A5/my-ecommerce-app/src/component/ProductList.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from './ProductItem'; // Adjust the import path as necessary
-import productsData from '../data/products'; // Adjust the import path as necessary
 
 function ProductList({ addToCart }) {
-    return (
-      <div>
-        {productsData.map(product => (
-          <ProductItem key={product.id} product={product} addToCart={addToCart} />
-        ))}
-      </div>
-    );
-  }
+    const [productsData, setProductsData] = useState([]);
+
+    // useEffect(() => {
+    //     fetch('/products')
+    //         .then(response => response.json())
+    //         .then(data => setProductsData(data));
+    // }, []);
   
-  export default ProductList;
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:5000/products')
+          .then(response => {
+              console.log(response);
+              return response.json();
+          })
+          .then(data => {
+              console.log(data);
+              setProductsData(data);
+          });
+        }, []);
+
+    return (
+        <div>
+            {productsData.map(product => (
+                <ProductItem key={product.id} product={product} addToCart={addToCart} />
+            ))}
+        </div>
+    );
+}
+
+export default ProductList;
+
