60345108f4bcd25e6eba601c8a76960a5ee2c607
SuprovaHoque <123154933+SuprovaHoque@users.noreply.github.com>
2024-04-04 10:45:27 -0600

    Fixed inconsistencies in displaying error messages

--- A4 and A5/my-ecommerce-app/src/component/LoginForm.js
+++ A4 and A5/my-ecommerce-app/src/component/LoginForm.js
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { LoginContext } from './LoginContext';
 
 const LoginForm = ({ switchToSignup }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [message, setMessage] = useState("");
-  const [authenticated, setAuthenticated] = useState(false);
   const navigate = useNavigate();
+  const { authenticated, setAuthenticated } = useContext(LoginContext);
 
   const handleLogin = () => {
     // Validate and handle login logic here
     if (username && password) {
-      console.log("Logged in successfully!");
-      setErrorMessage(""); // Clear the error message
-    } else {
-      setErrorMessage("Please fill in both fields.");
-    }
-
-    // Authenticating login
+      // Authenticating login
     fetch('http://127.0.0.1:5000/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -30,13 +26,18 @@ const LoginForm = ({ switchToSignup }) => {
         setMessage("Login successful")
       } else {
         setAuthenticated(false);
-        setMessage('Login failed. Incorrect username or password.');
+        setMessage('Incorrect username or password.');
       }
     }).catch(error => {
       console.error(error);
       setMessage('An error occurred.');
     });
-
+      console.log("Logged in successfully!");
+      setErrorMessage(""); // Clear the error message
+    } else {
+      setErrorMessage("Please fill in both fields.");
+      setMessage(''); // Clear any previous messages
+    }
   };
 
   // Redirect to the products page after successful login
@@ -44,11 +45,11 @@ const LoginForm = ({ switchToSignup }) => {
     navigate('/products')
   }
 
-
   return (
     <div>
       <h1>Login</h1>
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {message && <p style={{ color: "red" }}>{message}</p>}
       <label htmlFor="username">Username: </label>
       <input
         type="text"
@@ -68,7 +69,6 @@ const LoginForm = ({ switchToSignup }) => {
       <button onClick={handleLogin}>Login</button>
       <br />
       <button onClick={switchToSignup}>Switch to Signup</button>
-      {message && <p style={{ color: "red" }}>{message}</p>}
     </div>
   );
 };
