339d36be40c6d64c2e48482565edab4b0df801a0
Sarah10109 <hilybob10109@gmail.com>
2024-04-07 01:19:11 -0600

    debugging

--- A4 and A5/my-ecommerce-app/src/component/LoginForm.js
+++ A4 and A5/my-ecommerce-app/src/component/LoginForm.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from './LoginContext';
 
 const LoginForm = ({ switchToSignup }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const { authenticated, setAuthenticated } = useContext(LoginContext);
 
   const handleLogin = () => {
     // Validate and handle login logic here
     if (username && password) {
+      // Authenticating login
+    fetch('http://127.0.0.1:5000/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({'username': username, 'password': password})
+    }).then(response => response.json())
+    .then(response => {
+      if (response.authenticated) {
+        setAuthenticated(true);
+        setMessage("Login successful")
+        navigate('/products');
+      } else {
+        setAuthenticated(false);
+        setMessage('Incorrect username or password.');
+      }
+    }).catch(error => {
+      console.error(error);
+      setMessage('An error occurred.');
+    });
       console.log("Logged in successfully!");
-      // Add your login logic (e.g., API calls, authentication, etc.)
       setErrorMessage(""); // Clear the error message
     } else {
       setErrorMessage("Please fill in both fields.");
+      setMessage(''); // Clear any previous messages
     }
   };
 
@@ -20,6 +45,7 @@ const LoginForm = ({ switchToSignup }) => {
     <div>
       <h1>Login</h1>
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {message && <p style={{ color: "red" }}>{message}</p>}
       <label htmlFor="username">Username: </label>
       <input
         type="text"
--- A4 and A5/my-ecommerce-app/src/component/SignupForm.js
+++ A4 and A5/my-ecommerce-app/src/component/SignupForm.js
@@ -1,33 +1,58 @@
 // SignupForm.js
-
 import React, { useState } from 'react';
 
 const SignupForm = ({ switchToLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const [signupAuthenticated, setSignupAuthenticated] = useState(true);
+  
   const handleSignup = () => {
     // Validate and handle signup logic here
     if (username && password && confirmPassword && email) {
-        setErrorMessage('');
       if (password === confirmPassword) {
-        setErrorMessage('User signed up successfully!');
-        // Add your signup logic (e.g., API calls, user registration, etc.)
+        // Authenticating signup
+        fetch('http://127.0.0.1:5000/signup', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({'username': username, 'password': password, 'email': email})
+        }).then(response => response.json())
+          .then(response => {
+            if (!response.signupAuthenticated) {
+              setSignupAuthenticated(false);
+              setErrorMessage('');
+              setMessage("Username already exists.")
+            } else {
+              setSignupAuthenticated(true);
+              setMessage('User signed up successfully!');
+              setErrorMessage(''); // Clear any previous error messages
+              setUsername('');
+              setPassword('');
+              setConfirmPassword('');
+              setEmail('');
+            }
+          }).catch(error => {
+            console.error(error);
+            setMessage('An error occurred.');
+          });
       } else {
+        setMessage('');
         setErrorMessage('Passwords do not match!');
       }
     } else {
-        setErrorMessage('All fields are required.');
+      setMessage('');
+      setErrorMessage('All fields are required.');
     }
   };
-
+    
   return (
     <div>
         <h1>Signup</h1>
         {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        {message && <p style={{ color: "red" }}>{message}</p>}
 
         <label>Username: </label>
       <input
