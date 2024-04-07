5dcc6dc1e0a849c908f183961695f08429f55769
Sarah10109 <hilybob10109@gmail.com>
2024-04-07 01:18:06 -0600

    idk

--- A4 and A5/my-ecommerce-app/src/App.js
+++ A4 and A5/my-ecommerce-app/src/App.js
@@ -1,19 +1,23 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './component/Homepage'; // Adjust the import path as necessary
-import Productpage from './component/Productpage'; // Import Productpage component
+import { AuthProvider } from './component/LoginContext';
+import Homepage from './component/Homepage';
+import Productpage from './component/Productpage';
 import LoginPage from './component/LoginPage';
+import PrivateRoute from './component/PrivateRoute';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} /> {/* Homepage includes Header and Footer */}
-        {/* Define routes for other components, without including Header and Footer, as they are part of Homepage */}
-        <Route path="/products" element={<Productpage />} /> {/* Use Productpage for the products route */}
-        <Route path="/login" element={<LoginPage />} /> {/* LoginPage includes Header and Footer */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<PrivateRoute><Productpage /></PrivateRoute>} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
